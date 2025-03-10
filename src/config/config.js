282e76afcfd3ba8afeb51ad69eb7a072324a69a0
Mohammad Murra <murra@birdie.ps>
2025-03-10 02:37:51 +0200

    update database mysql port config

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
 
 module.exports = {
   development: {
@@ -6,15 +8,17 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT || 3306, // Use MYSQL_PORT from your env file
     dialect: "mysql",
-    logging: console.log
+    logging: console.log,
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT || 3306,
     dialect: "mysql",
-    logging: false
-  }
+    logging: false,
+  },
 };

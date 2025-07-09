da2fa1009b489c0e1339da20a6904a836d8c4f2f
mohammad murra <mabumurra@gmail.com>
2025-07-09 20:57:39 +0300

    DB_Host to MYSQL_HOST

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
+// config/config.js
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV || "development"}`,
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,5 @@
+
+// config/database.js
 const { Sequelize } = require("sequelize");
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV || "development"}`,
